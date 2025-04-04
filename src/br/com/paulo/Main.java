f0191f17d7117dea7901e1e67d13db7763eb3866
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-04-04 08:14:41 -0300

    feat: finished final task

--- src/br/com/paulo/Main.java
+++ src/br/com/paulo/Main.java
@@ -3,23 +3,38 @@ package br.com.paulo;
 import br.com.paulo.animais.Leao;
 import br.com.paulo.animais.Passaro;
 import br.com.paulo.animais.Peixe;
+import br.com.paulo.interfaceanimal.Animal;
+
+import java.util.ArrayList;
 
 public class Main {
+    private static final ArrayList<Animal> animalLista = new ArrayList<>();
+
     public static void main(String[] args) {
         Peixe tilapia = new Peixe();
+        animalLista.add(tilapia);
+
         Leao leao = new Leao();
+        animalLista.add(leao);
+
         Passaro sabia = new Passaro();
+        animalLista.add(sabia);
 
-        tilapia.fazerSom();
-        tilapia.movimentar();
-
-
-        leao.fazerSom();
-        leao.movimentar();
-
-        sabia.fazerSom();
-        sabia.movimentar();
+        exibirSomDosAnimais();
+        movimentarAnimal();
 
 
+    }
+
+    private static void exibirSomDosAnimais(){
+        for(Animal animal: animalLista){
+            animal.fazerSom();
+        }
+    }
+
+    private static void movimentarAnimal(){
+        for(Animal animal: animalLista){
+            animal.movimentar();
+        }
     }
 }
